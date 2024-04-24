b1f12b339c6a954271c51652979926dd1a9f2385
Shawn Converse <shawn.a.converse@outlook.com>
2024-04-24 14:49:09 -0700

    some more changes

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,19 +1,17 @@
 function ColorMe(color)
-    color = color or "kanagawa"
+    color = color or "melange"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-
 return {
-	"rebelot/kanagawa.nvim",
-	config = function()
-		require("kanagawa").setup({})
+    -- "rebelot/kanagawa.nvim",
+    "savq/melange-nvim",
+    config = function()
+        -- require("kanagawa").setup({})
 
         ColorMe()
-	end,
+    end,
 }
-
-
